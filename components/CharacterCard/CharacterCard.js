98befd66793b2c90299d3472f15fb3ff545efa0e
Marie Langer <98027776+marielngr@users.noreply.github.com>
2023-12-11 11:10:02 +0100

    finish CharacterCard

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,40 +1,29 @@
 export function CharacterCard(props) {
   const card = document.createElement("li");
   const cardContainer = document.querySelector(`[data-js="card-container"]`);
-
   card.classList.add("card");
-  card.innerHTML = `<div class="card__image-container">
+  card.innerHTML = `
+  <div class="card__image-container">
   <img class="card-image"
-  src= "props.img"
-  alt = "props.name" />`;
+    src= ${props.image}
+    alt = ${props.name} />
+  <div class="card__image-gradient"></div>
+  </div>
+  <div class="card__content">
+     <h2 class="card__title">${props.name}</h2>
+  <dl class="card__info">
+    <dt class="card__info-title">Status</dt>
+    <dd class="card__info-description">${props.status}</dd>
+    <dt class="card__info-title">Type</dt>
+    <dd class="card__info-description">${props.type}</dd>
+    <dt class="card__info-title">Occurrences</dt>
+    <dd class="card__info-description">${props.episode}</dd>
+  </dl>
+</div>
+  `;
   cardContainer.append(card);
 
   return card;
 }
 
 CharacterCard();
-
-// //</div>
-// //      <ul class="card-container" data-js="card-container">
-// //      <li class="card">
-// //        <div class="card__image-container">
-//  //           <img
-//   //            class="card__image"
-//     //          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-//       //        alt="Rick Sanchez"
-//         //    />
-//           //  <div class="card__image-gradient"></div>
-//  //         </div>
-//    //       <div class="card__content">
-//      //       <h2 class="card__title">Rick Sanchez</h2>
-//             <dl class="card__info">
-//               <dt class="card__info-title">Status</dt>
-//               <dd class="card__info-description">Alive</dd>
-//               <dt class="card__info-title">Type</dt>
-//               <dd class="card__info-description"></dd>
-//               <dt class="card__info-title">Occurrences</dt>
-//               <dd class="card__info-description">51</dd>
-//             </dl>
-//           </div>
-//</li>
-//    </ul>
