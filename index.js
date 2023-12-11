6c84c42c48d94a65b168a6f06a8d86367f7e54ef
Marie Langer <98027776+marielngr@users.noreply.github.com>
2023-12-11 23:01:26 +0100

    ein bisschen von task 3 noch geschafft

--- index.js
+++ index.js
@@ -11,20 +11,36 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
 
-async function fetchCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
-  const data = await response.json();
-  return data.results;
+//info: 42 Seiten
+
+async function fetchCharacters(page) {
+  try {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${page}`
+    );
+    const data = await response.json();
+    console.log(data);
+    maxPage = data.info.pages;
+    return data.results;
+  } catch (error) {
+    alert("Fehler beim Abrufen der Daten");
+    return null;
+  }
 }
 
-const characters = await fetchCharacters();
+const characters = await fetchCharacters(page);
 console.log(characters);
 
 characters.forEach((character) => {
   const card = CharacterCard(character);
   cardContainer.append(card);
 });
+
+//Hier Event-Listener
+// - it is prevented that the page index could go higher than the max page index or below 1
+// - the page index is increased / decreased
+// - the `fetchCharacters` function is called
